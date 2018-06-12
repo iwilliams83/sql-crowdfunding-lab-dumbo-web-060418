116a9124b1ec9aa9803014cdad06c501d3c22347
iwilliams83 <indirajamurath@gmail.com>
2018-06-12 14:08:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, pledges.amounts FROM projects INNER JOIN pledges on projects.id = pledges.project_id WHERE projects.category = 'music'"
+"SELECT projects.category, pledges.amount FROM projects INNER JOIN pledges on projects.id = pledges.project_id WHERE projects.category = 'music'"
 #[["music", 40], ["music", 24], ["music", 34], ["music", 12], ["music", 40], ["music", 40], ["music", 20], ["music", 230]]
 end
 
